801ea8582aadae5f3729c1fa32bf024ce3d74251
Vinicius Bomfim <vbomfim@gmail.com>
2026-02-07 16:36:16 -0500

    feat: project setup with NuGet packages and folder structure (#1)

--- src/CopilotVoice/Program.cs
+++ src/CopilotVoice/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CopilotVoice;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Copilot Voice - Push-to-talk for GitHub Copilot CLI");
+        Console.WriteLine("Run with --help for options");
+    }
+}
